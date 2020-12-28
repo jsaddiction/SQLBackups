d7d964cd17ac3dd12fe3b3f647b5b012e47faccc
jsaddiction <04reduramax@gmail.com>
2020-12-28 17:40:22 -0500

    make list of dbs

--- SQLBackups.py
+++ SQLBackups.py
@@ -25,7 +25,7 @@ def getDatabases():
         LOG.debug('Failed to get database list. Error: {}'.format(e))
         return []
 
-    return result.stdout.decode('UTF-8')
+    return list(result.stdout.decode('UTF-8'))
     
 
 def backupDB(dbName):
